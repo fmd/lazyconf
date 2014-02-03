f9e91f34b5bd8fd53c76c7b6de4f6e0d2da7aae5
Fareed Dudhia <fareeddudhia@googlemail.com>
2014-02-03 14:47:39 +0000

    Bulk of original wotsit done

--- lazyconf/lazyconf.py
+++ lazyconf/lazyconf.py
@@ -1,13 +1,72 @@
 import os
 import json
 from fabric.api import *
+from distutils.util import strtobool
 from fabric.colors import green, red
 
-class Ezconf():
-    def __init__(self, project_dir, filename = 'config.json'):
+class Lazyconf():
+
+    def config(self):
+        self.load()
+        self.__cfg(self.data, 'config')
+        self.save()
+
+    def save_schema(self):
+        d = self.data
+        self.__sv_schema(d)
+
+    def __sv_schema(self, d):
+        for k,v in d.iteritems():
+            if t is dict:
+                self.__sv_schema(d)
+            elif t is bool:
+                v = False
+            elif t is int:
+                v = 0
+            else:
+                v = ''
+        return
+
+    def __get_label(self, l):
+        if l in self.data['labels'].keys():
+            return self.data['labels'][l]
+        return l
+
+    def __cfg(self, d, key_string):
+
+        if 'enabled' in d.keys():
+            s = '.'.join([key_string,'enabled'])
+            d['enabled'] = self.deprompt_bool(prompt(self.__get_label(s), default = self.prompt_bool(d['enabled'])))
+            if d['enabled'] is False:
+                return
+
+        for k, v in d.iteritems():
+            if key_string == 'config.labels':
+                return
+
+            if k == 'enabled':
+                continue
+            
+            t = type(v)
+            s = '.'.join([key_string,k])
+
+            if t is dict:
+                self.__cfg(v, s)
+            elif t is str or t is unicode:
+                d[k] = prompt(self.__get_label(s), default = v)
+            elif t is bool:
+                d[k] = self.deprompt_bool(prompt(self.__get_label(s), default = self.prompt_bool(v)))
+            elif t is int:
+                d[k] = prompt(self.__get_label(s), default = v)
+
+    def __init__(self, project_dir, filename = 'lazyconf.json'):
 
         ### Project directory and filename ###
+        if len(project_dir) == 0:
+            project_dir = '.'
+
         self.project_dir = project_dir
+
         self.filename = filename
 
         ### Dictionary of available database engines ###
@@ -21,58 +80,77 @@ class Ezconf():
             'memcached' : 'django.core.cache.backends.memcached.MemcachedCache',
         }
 
-        ### Skeletal config.json in dictionary form ###
-        self.data = {
-            'cache' : {
-                'enabled'  : False,
-                'backend'  : 'memcached',
-                'location' : '127.0.0.1:11211',
-            },
+        ### Skeletal lazyconf.json in dictionary form ###
+        self.data = {}
 
-            'project' : {
-                'name'        : '',
-                'base_dir'    : '',
-                'project_dir' : '',
-            },
+    def load(self, path = None):
+        if not self.load_file(path):
+            if not self.load_file(path, True):
+                if not self.load_file('./' + self.filename, True):
+                    print(red("Fatal: Could not load JSON file, schema, or local schema. Aborting..."))
+                    exit()
 
-            'env': {
-                'debug' : True,
-            },
-
-            'db' : {
-                'engine' : '',
-                'user'   : '',
-                'name'   : '',
-                'pass'   : '',
-            },
-        }
-
-    ### Loading from and saving to file ###
-    def load(self):
+    def load_file(self, path = None, schema = False):
+        if path is None:
+            path = self.project_dir + '/' + self.filename
+        if schema:
+            path += '.schema'
         try:
-            with open(self.project_dir + '/' + self.filename) as handle:
-            self.data = json.load(handle)
-            handle.close()                
-        except IOError:
-            self.save()
-            return
+            with open(path) as handle:
+                print(green("Loading JSON from " + path))
+                try:
+                    self.data = json.load(handle)
+                except ValueError as e:
+                    print(red('Error parsing JSON: ' + str(e)))
+                    return None
+                if type(self.data) is not dict:
+                    print(red('Error parsing JSON from file ' + path))
+                    return None
+                handle.close()
+        except IOError as e:
+            print(red('Could not load file: ' + str(e)))
+            return None
 
-    def save(self):
-        with open(self.project_dir + '/' + self.filename, 'w') as handle:
-            json.dump(self.data, handle, indent=4)
+        return True
+
+    def save(self, d = None, f = None):
+        if not d:
+            d = self.data
+        if not f:
+            f = self.filename
+        try:
+            with open(self.project_dir + '/' + f, 'w') as handle:
+                json.dump(d, handle, indent=4)
+        except IOError as e:
+            print(red('Could not save file: ' + str(e)))
+            exit()
+        else:
+            print(green('Saved data to file: ' + f))
 
     ### Prompt/Deprompt functions ###
     def prompt_db_engine(self, p):
-        pass
+        for key, engine in self.db_engines.iteritems():
+            if p == engine:
+                return key
+        return ''
 
     def deprompt_db_engine(self, p):
-        pass
+        engine = self.db_engines[p]
+        if len(engine) > 0:
+            return engine
+        return None
 
     def prompt_cache_backend(self, p):
-        pass
+        for key, backend in self.cache_backends.iteritems():
+            if p == backend:
+                return key
+        return ''
 
-    def depromp_cache_backend(self, p):
-        pass
+    def deprompt_cache_backend(self, p):
+        engine = self.cache_backends[p]
+        if len(engine) > 0:
+            return engine
+        return None
 
     def prompt_bool(self, p):
         if p is True:
@@ -82,9 +160,11 @@ class Ezconf():
         return ''
 
     def deprompt_bool(self, p): 
-        if p == 'y':
+        if p.lower() in ['y','yes','true']:
             return True
-        if p == 'n':
+        if p.lower() in ['n','no','false']:
             return False
         return None
 
+lz = Lazyconf(os.path.dirname(__file__))
+lz.config()
