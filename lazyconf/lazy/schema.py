8cabfb3adf58238e6c4667449bfc6ccfaebfe474
Fareed Dudhia <fareeddudhia@googlemail.com>
2014-02-04 15:26:05 +0000

    schema comment changes

--- lazyconf/lazy/schema.py
+++ lazyconf/lazy/schema.py
@@ -7,38 +7,52 @@ class Schema():
     # Initialisation 
     def __init__(self):
         self.data = None
-        self.prefix = prefix
 
+    # Gets a value from dot format: s.get('project.cache.backend') in dict format: self.data['project']['cache']['backend']
     def get(self, key):
+
         if not self.data:
             return None
-        
+
+        # Split the key by its dots.        
         parts = key.rsplit('.')
+
+        # Loop through the key.
         v = self.data
         for p in parts:
+            
+            # Try and get the value for this section in the key.
             try:
                 v = v.get(p)
+
+            # If the key is invalid, return None rather than throwing an exception.
             except AttributeError as e:
                 return None
 
+        # If we got the value successfully, return it.
         return v
 
+    # Loads self.data from a file.
     def load(self, path):
         data = None
 
+        # Try and open the file, or raise an exception.
         try:
             handle = open(path)
         except IOError as e:
             raise e
 
+        # Try to load valid JSON, or raise an exception.
         try:
             data = json.load(handle)
         except ValueError as e:
             raise e
 
+        # Ensure that the JSON is at least a dictionary, or raise an exception.
         if type(data) is not dict:                    
             raise Exception("Invalid schema")
 
+        # Store the dictionary.
         handle.close()
         self.data = data
 
