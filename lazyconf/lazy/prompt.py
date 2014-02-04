d8e2afb2c625fdd7efbeb152bd97aba9fd9de250
Fareed Dudhia <fareeddudhia@googlemail.com>
2014-02-04 20:34:33 +0000

    Added comments and defined some functions in prompt

--- lazyconf/lazy/prompt.py
+++ lazyconf/lazy/prompt.py
@@ -1,18 +1,35 @@
 from fabric.api import *
 from fabric.colors import green, red, blue
 
-# Prompt
-# This class contains several helper functions for getting data between end-user's input and a schema.Schema object.
+### Prompt ###
+### This class contains several helper functions for getting data between end-user's input and a schema.Schema object.
+### It also containers several formatting functions, which are currently just a convenience wrapper around printing Fabric colors.
 
 class Prompt():
     def __init__(self):
         pass
 
+    ### Formatting ###
+
+    # Prints a success message.
     def success(self, msg):
         pass
 
+    # Prints an error message.
     def error(self, msg):
         pass
 
+    # Prints a notice message.
     def notice(self, msg):
         pass
+
+    ### Parsing ###
+
+    # Takes a python bool and returns it as y|n.
+    def prompt_bool(self, b):
+        pass
+
+    # Takes y|n and returns a python bool.
+    def deprompt_bool(self, s):
+        pass
+
