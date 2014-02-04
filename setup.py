a5ba224d270483b8587f0766b1584fd415e4e98a
Fareed Dudhia <fareeddudhia@googlemail.com>
2014-02-04 00:38:42 +0000

    Updated name in setup file

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
 setup(
-    name='django-lazyconf',
+    name='lazyconf',
     version='0.1',
     author='Fareed Dudhia',
     author_email='fareeddudhia@gmail.com',
     packages=['lazyconf'],
     scripts=[],
-    url='https://www.github.com/fmd/django-lazyconf',
+    url='https://www.github.com/fmd/lazyconf',
     license='LICENSE.rst',
     description='Lazy config and deployment for Django.',
     long_description=open('README.rst').read(),
