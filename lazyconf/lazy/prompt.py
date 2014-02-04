af6a4d838e1064d6194e4ebd9f6762aef6262a08
Fareed Dudhia <fareeddudhia@googlemail.com>
2014-02-04 23:25:36 +0000

    Added prompt bool functions

--- lazyconf/lazy/prompt.py
+++ lazyconf/lazy/prompt.py
@@ -25,13 +25,29 @@ class Prompt():
     def notice(self, msg):
         print(blue(msg))
 
-    ### Parsing ###
+    ### Prompts ###
+
+    # Returns the value from a validated bool prompt.
+    def prompt_bool(self, label, default = False):
+        val = prompt(label + '? (y/n)', default = self.fmt_bool(default), validate = r'^(y|n)$')
+        return self.defmt_bool(val)
+
+    ### Formatting ###
 
     # Takes a python bool and returns it as y|n.
-    def prompt_bool(self, b):
-        pass
+    def fmt_bool(self, p):
+        if p is True:
+            return 'y'
+        if p is False:
+            return 'n'
+        return ''
 
     # Takes y|n and returns a python bool.
-    def deprompt_bool(self, s):
-        pass
+    def defmt_bool(self, p): 
+        if p == 'y':
+            return True
+        if p == 'n':
+            return False
+        return None
+
 
