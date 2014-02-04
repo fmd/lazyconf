50664c1633576dac7c327b41761a54543601126b
Fareed Dudhia <fareeddudhia@googlemail.com>
2014-02-04 00:24:31 +0000

    Updated setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     description='Lazy config and deployment for Django.',
     long_description=open('README.rst').read(),
     install_requires=[
-        "Django >= 1.7",
+        "Django >= 1.4",
         "Fabric >= 1.8.1"
     ],
 )
