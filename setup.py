1ed6201a3e59a8a6a02986dd2963cad127a55bd4
Fareed Dudhia <fareeddudhia@googlemail.com>
2014-02-04 00:08:06 +0000

    Updated setup.py with correct README name

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 
 setup(
     name='django-lazyconf',
-    version='0.1.0',
+    version='0.1',
     author='Fareed Dudhia',
     author_email='fareeddudhia@gmail.com',
     packages=['lazyconf'],
     scripts=[],
     url='https://www.github.com/fmd/django-lazyconf',
-    license='LICENSE.txt',
+    license='LICENSE.rst',
     description='Lazy config and deployment for Django.',
     long_description=open('README.txt').read(),
     install_requires=[
