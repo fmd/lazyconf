ce7782c72fc99749f9ce1c03f3dfb676b52f39fa
Fareed Dudhia <fareeddudhia@googlemail.com>
2014-02-04 15:09:07 +0000

    added some skeletons defs to prompt

--- lazyconf/lazy/prompt.py
+++ lazyconf/lazy/prompt.py
@@ -1,5 +1,18 @@
+from fabric.api import *
+from fabric.colors import green, red, blue
+
 # Prompt
 # This class contains several helper functions for getting data between end-user's input and a schema.Schema object.
 
 class Prompt():
-    pass
+    def __init__(self):
+        pass
+
+    def success(self, msg):
+        pass
+
+    def error(self, msg):
+        pass
+
+    def notice(self, msg):
+        pass
