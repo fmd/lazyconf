42cb690607d63e4bc20045432a81e2b07e3cfb37
Fareed Dudhia <fareeddudhia@googlemail.com>
2014-02-06 01:32:12 +0000

    Hopefuly fixed travis dirs for the last time

--- lazyconf/test.py
+++ lazyconf/test.py
@@ -2,9 +2,5 @@ import os
 from lazyconf import Lazyconf
 
 l = Lazyconf()
-path = os.path.dirname(__file__)
-if len(path) == 0:
-    path = '.'
-
-l.load(path + '/lazyconf.json')
+l.load(path + './lazyconf/lazyconf.json')
 print l.get('db.user')
