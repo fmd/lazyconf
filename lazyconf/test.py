d883695df9614649d3d916ca17112ae7b0af53bc
Fareed Dudhia <fareeddudhia@googlemail.com>
2014-02-06 01:26:35 +0000

    test now works on travis

--- lazyconf/test.py
+++ lazyconf/test.py
@@ -2,5 +2,9 @@ import os
 from lazyconf import Lazyconf
 
 l = Lazyconf()
-l.load(os.path.dirname(__file__) + '/lazyconf.json')
+path = os.path.dirname(__file__)
+if len(path) == 0:
+    path = '.'
+
+l.load(path + '/lazyconf.json')
 print l.get('db.user')
