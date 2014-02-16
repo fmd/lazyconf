dc6569e3faed90c2262378b78002c97d748c153f
Fareed Dudhia <fareeddudhia@googlemail.com>
2014-02-16 02:48:49 +0000

    getting there with .set

--- lazyconf/console.py
+++ lazyconf/console.py
@@ -14,10 +14,39 @@ def parse():
     sp_config = sp.add_parser('config', help='Run the configurator.')
     
     sp_get = sp.add_parser('get', help='Gets the value for a specific key in the configuration.')
+    sp_set = sp.add_parser('set', help='Sets the value for a specific key in the configuration.')
     sp_get.add_argument('-k', '--key',
-                        help='The key to retrieve.', dest='key')
-
+                        help='The key to retrieve.', dest='key', required=True)
+    sp_set.add_argument('-k', '--key',
+                        help='The key to set for.', dest='key', required=True)
+    sp_set.add_argument('-v', '--value',
+                        help='The value to set.', dest='value', required=True)
+    
     return parser.parse_args()
+
+def get(l, key):
+    try:
+        l.load()
+    except:
+        exit()
+    
+    r = l.get(key)
+    if r:
+        print r
+    exit()
+
+def set(l, key, value):
+    try:
+        l.load()
+    except Exception as e:
+        raise  e
+
+    try:
+        l.set(key, value)
+    except Exception as e:
+        raise e
+
+    exit()
 
 def console():
     command_dict = parse().__dict__
@@ -26,17 +55,14 @@ def console():
     l = lazyconf.Lazyconf()
 
     if command == 'get':
-        try:
-            l.load()
-        except:
-            exit()
-        r = l.get(command_dict['key'])
-        if r:
-            print r
-        exit()
-
+        key = command_dict['key']
+        get(l, key)
     elif command == 'config':
         l.configure()
+    elif command == 'set':
+        key = command_dict['key']
+        value = command_dict['value']
+        set(l, key, value)
        
 # Call the console function if we call this file directly.
 if __name__ == '__main__':
