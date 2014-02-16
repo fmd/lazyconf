02cfdb40672f1da10d34654dea3c27d1513e275e
Fareed Dudhia <fareeddudhia@googlemail.com>
2014-02-16 15:29:23 +0000

    updated setup py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ s = 'schema/'
 
 setup(
     name='lazyconf',
-    version='0.4.6',
+    version='0.5.0',
     author='Fareed Dudhia',
     author_email='fareeddudhia@gmail.com',
     packages=find_packages(),
